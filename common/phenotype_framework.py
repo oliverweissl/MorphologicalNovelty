c6ba8179899523591db15a25ce423cd59889a281
Oliver <o.weissl@vu.nl>
2023-03-26 18:41:29 +0200

    added PCA change of basis, optimized orientation, magnitude extraction

--- common/phenotype_framework.py
+++ common/phenotype_framework.py
@@ -5,6 +5,9 @@ import numpy as np
 from multineat import Genome
 from typing import List, Tuple
 from math import atan2, pi, sqrt
+
+from numpy import ndarray
+
 from .compare_histograms import CompareHistorgrams as ch
 from revolve2.genotypes.cppnwin._genotype import Genotype
 from revolve2.core.modular_robot import Body, ActiveHinge, Brick
@@ -14,7 +17,7 @@ from revolve2.genotypes.cppnwin.modular_robot.body_genotype_v1 import develop_v1
 class PhenotypeFramework:
 
     @classmethod
-    def get_blocks_hinges_amount(cls, genotype: Genotype | str) -> (int, int):
+    def get_bricks_hinges_amount(cls, genotype: Genotype | str) -> (int, int):
         assert isinstance(genotype, (Genotype, str)), f"Error: Genotype is of type {type(genotype)}, Genotype or str expected!"
 
         genotype = genotype if not isinstance(genotype, str) else cls.deserialize(genotype)
@@ -35,19 +38,24 @@ class PhenotypeFramework:
 
         genotypes = [genotype if not isinstance(genotype, str) else cls.deserialize(genotype) for genotype in genotypes]
         bodies = [develop_v1(genotype) for genotype in genotypes]
+
         coords = [cls._body_to_sorted_coordinates(body) for body in bodies]
+        coords = [cls._coordinates_pca_change_basis(coord) for coord in coords] # PCA change of basis -> orientation of variance/ covariance
+
 
         brick_hists = [None] * amt_instances
         hinge_hists = [None] * amt_instances
 
         i = 0
         for coord in coords:
-            brick_hists[i] = cls._gen_gradient_histogram(magnitudes=cls._coordinates_to_magnitudes(coord[0]),
-                                                         orientations=cls._coordinates_to_orientation(coord[0]),
+            brick_mag, brick_orient = cls._coordinates_to_magnitudes_orientation(coord[0])
+            brick_hists[i] = cls._gen_gradient_histogram(magnitudes=brick_mag,
+                                                         orientations=brick_orient,
                                                          normalization=normalization)
 
-            hinge_hists[i] = cls._gen_gradient_histogram(magnitudes=cls._coordinates_to_magnitudes(coord[1]),
-                                                         orientations=cls._coordinates_to_orientation(coord[1]),
+            hinge_mag, hinge_orient = cls._coordinates_to_magnitudes_orientation(coord[1])
+            hinge_hists[i] = cls._gen_gradient_histogram(magnitudes=hinge_mag,
+                                                         orientations=hinge_orient,
                                                          normalization=normalization)
             i += 1
 
@@ -62,9 +70,11 @@ class PhenotypeFramework:
                 hinge_score = cls._compare_hist(hinge_hists[i], hinge_hists[j], novelty_test)
                 hinge_novelty_scores[i] += hinge_score
                 hinge_novelty_scores[j] += hinge_score
+                #print(f"Instances: {i}, {j}: brick_score: {brick_score}, hinge_score:  {hinge_score}")
 
         novelty_scores = [(b_score + h_score) / 2
                           for b_score, h_score in zip(brick_novelty_scores, hinge_novelty_scores)]
+
 
         novelty_scores = cls._normalize_list(novelty_scores, "scaling")
         # scaling because the min novelty is 0 in theory --> some populations can have no duplicates therefore no 0s
@@ -90,7 +100,7 @@ class PhenotypeFramework:
                      'manhattan-dist': ch.manhattan_distance,
                      'euclidian-dist': ch.euclidian_distance,
                      'chybyshev-dist': ch.chybyshev_distance,
-                     'pcc': ch.pearsons_correlation_coefficient
+                     'pcc': ch.pearsons_correlation_coefficient,
                      }[test](O, E)
         return score
 
@@ -108,6 +118,7 @@ class PhenotypeFramework:
 
         x_size, y_size, z_size = body_arr.shape
 
+
         flat_body_arr = body_arr.flatten()
         flat_body_coords = np.asarray([[[
             tuple(np.subtract((x, y, z), core_pos)) for x in range(x_size)]
@@ -124,44 +135,49 @@ class PhenotypeFramework:
         return bricks, hinges
 
     @classmethod
-    def _coordinates_to_magnitudes(cls, coordinates: List[Tuple]) -> List[float]:
-        """
-        Returns array of vector magnitudes for array of coordinate tuples.
-        Accepts List[Tuple] Tuple -> (x,y,z)
-        :param coordinates:
-        :return: List of magnitudes (Float)
-        """
-        mags = [None] * len(coordinates)  # faster than append
-        i = 0  # faster than enumerate
-        for elem in coordinates:
-            elem = np.asarray(list(elem))
-            mags[i] = np.sqrt(elem.dot(elem))
-            i += 1
-        return mags
+    def _coordinates_pca_change_basis(cls, coords: Tuple[List[Tuple], List[Tuple]]) -> (ndarray, ndarray):
+        bricks, hinges = coords
+        bricks = np.asarray([list(b) for b in bricks])
+        hinges = np.asarray([list(h) for h in hinges])
+
+        all_coords = []
+        [all_coords.append(val) for val in bricks if len(bricks) > 0]
+        [all_coords.append(val) for val in hinges if len(hinges) > 0]
+
+        if len(all_coords) > 1: #has to be more than 1 block, otherwise np.cov doesnt work
+            all_coords = np.asarray(all_coords).T
+
+            A = np.cov(all_coords)
+            e, v = np.linalg.eig(np.dot(A, A.T)/(len(all_coords)-1)) # eigenvalues, eigenvectors
+            srt = np.argsort(-e) #sorting axis, x-axis: biggest variance, y-axis second biggest, z-axis:smallest
+            inv_sorted_v = np.linalg.inv(v[srt].T)
+
+            bricks = np.dot(inv_sorted_v, bricks.T) if len(bricks) > 0 else bricks
+            hinges = np.dot(inv_sorted_v, hinges.T) if len(hinges) > 0 else hinges
+            bricks, hinges = bricks.T, hinges.T
+
+
+
+        return bricks, hinges
 
     @classmethod
-    def _coordinates_to_orientation(cls, coordinates: List[Tuple]) -> List[Tuple[float, float]]:
-        """
-        Calcuclates the orientation of a vector with respect to the x and z axis.
-        y-axis is obsolete, since its x- 90deg.
-        Results in a tuple of (angle-x, angle-y), measured in radiants.
-        :param coordinates:
-        :return:
-        """
+    def _coordinates_to_magnitudes_orientation(cls, coordinates: List[Tuple]) -> Tuple[List[float], List[Tuple[float, float]]]:
+        mags = [None] * len(coordinates)  # faster than append
+        orient = [None] * len(coordinates)  # faster than append
         i = 0  # faster than enumerate
-        rot_vec = [None] * len(coordinates)  # faster than append
         for elem in coordinates:
             elem = list(elem)
-            # math faster than np in this case
             ax = atan2(sqrt(elem[1] ** 2 + elem[2] ** 2), elem[0]) * 180 / pi
             az = atan2(elem[2], sqrt(elem[1] ** 2 + elem[0] ** 2)) * 180 / pi
+            orient[i] = (ax, az)
 
-            rot_vec[i] = (ax, az)
+            elem = np.asarray(elem)
+            mags[i] = np.sqrt(elem.dot(elem))
             i += 1
-        return rot_vec
+        return mags, orient
 
     @classmethod
-    def _gen_gradient_histogram(cls, magnitudes: List[float], orientations: List[Tuple[float, float]], normalization: str | None, num_bins: int = 18) -> List[List[float]]:
+    def _gen_gradient_histogram(cls, magnitudes: List[float], orientations: List[Tuple[float, float]], normalization: str | None, num_bins: int = 18) -> ndarray:
         """
         Generates a 2D-Historgram of oriented gradients, using bins to standardize feature size. Can be normalized in various ways to make it comparable.
         :param magnitudes: Magnitudes List[float]
@@ -186,7 +202,7 @@ class PhenotypeFramework:
         return hist
 
     @staticmethod
-    def _normalize_list(lst: list | np.array, normalization: str | None) -> list | np.array:
+    def _normalize_list(lst: list | np.array, normalization: str | None) -> ndarray:
         new_lst = np.asarray(lst)
         max_val, min_val = new_lst.max(), new_lst.min()
 
