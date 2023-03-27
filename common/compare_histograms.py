c672ef7df1c3602589acd7b9bf2cdbb867d58436
Oliver <o.weissl@vu.nl>
2023-03-27 21:07:54 +0200

    wasserstein-distance added

--- common/compare_histograms.py
+++ common/compare_histograms.py
@@ -4,8 +4,9 @@ import numpy as np
 from itertools import chain
 import time
 from math import sqrt
-
 from numpy import ndarray
+
+INT_CASTER = 10_000
 
 
 class CompareHistorgrams:
@@ -126,15 +127,28 @@ class CompareHistorgrams:
 
     @classmethod
     def wasserstein_dist(cls, O: List[List[float]], E: List[List[float]]) -> float:
-        # https://www.cs.cmu.edu/~efros/courses/LBMV07/Papers/rubner-jcviu-00.pdf
+        """
+        the wasserstein distance quantifies, how similar two distributions are.
+        This implementation uses a INT_CASTER, to mitigate floating point calculation errors.
+        A distribution needs to sum up to 1 for the algorithm to finnish.
+        --> the INT_CASTER is a multiplicator of such
+        --> casting to int leaves margin -> margin is added back as random noise on the dist.
+
+        literature: https://www.cs.cmu.edu/~efros/courses/LBMV07/Papers/rubner-jcviu-00.pdf
+        :param O: a distribution of size NxN
+        :param E: a distribution of size NxN
+        :return: float value of novelty score
+        """
+
         O, E = np.asarray(O), np.asarray(E)
         assert O.shape == E.shape, f"Error: Histograms have different sizes -> O:{O.shape}, E:{E.shape}"
         assert round(O.sum()) == round(E.sum()) == 1., f"Error: Distributions  dont sum up to 1 -> O:{round(O.sum())}, E:{round(E.sum())}"
         xsize, ysize = O.shape
 
-        supply, capacity = np.copy(O), np.copy(E)
-        score = 0
+        supply, capacity = np.copy(O*INT_CASTER).astype(int), np.copy(E*INT_CASTER).astype(int)
+        supply, capacity = cls._apply_noise_mask(supply), cls._apply_noise_mask(capacity)
 
+        score = 0
         while True:
             from_idx = (supply != 0).argmax()
             fx, fy = int(from_idx / ysize), from_idx % xsize
@@ -142,12 +156,26 @@ class CompareHistorgrams:
             to_idx = (capacity != 0).argmax()
             tx, ty = int(to_idx / ysize), to_idx % xsize
 
-            if round(supply[fx][fy]) == 0 or round(capacity[tx][ty]) == 0:
+            if supply[fx][fy] == 0 or capacity[tx][ty] == 0:
                 break
 
-            work, supply, capacity = cls._move_supply(supply, fy, fy, capacity, tx, ty)
+            work, supply, capacity = cls._move_supply(supply, fx, fy, capacity, tx, ty)
             score += work
-        return score
+        return float(score)
+
+    @classmethod
+    def _apply_noise_mask(cls, dist: ndarray) -> ndarray:
+        xsize, ysize = dist.shape
+        diff = INT_CASTER - dist.sum()
+        mask = np.zeros(xsize * ysize)
+        mask[:diff] = 1
+        np.random.shuffle(mask)
+
+        mask = mask.reshape((xsize, ysize))
+
+        dist = np.add(dist, mask)
+        assert dist.sum() == INT_CASTER, f"ERROR: Sum of Dist is expected to be: {INT_CASTER} ,but is: {dist.sum()}"
+        return dist
 
     @staticmethod
     def _move_supply(selected_supply: ndarray, sx: int, sy: int,
@@ -155,14 +183,14 @@ class CompareHistorgrams:
 
         if selected_supply[sx][sy] <= selected_capacity[cx][cy]:
             flow = selected_supply[sx][sy]
-            selected_supply[sx][sy] = 0.0
             selected_capacity[cx][cy] -= flow
-        elif selected_supply[sx][sy] > selected_capacity[cx][cy]:
+            selected_supply[sx][sy] = 0
+        else:
             flow = selected_capacity[cx][cy]
             selected_supply[sx][sy] -= flow
-            selected_capacity[cx][cy] = 0.0
-        dist = sqrt(np.abs(sx - cx) ** 2 + np.abs(sy - cy) ** 2)
-        return flow * dist, selected_supply, selected_capacity
+            selected_capacity[cx][cy] = 0
+        distance = sqrt(np.abs(sx - cx) ** 2 + np.abs(sy - cy) ** 2)
+        return flow/INT_CASTER*distance, selected_supply, selected_capacity
 
     @abstractmethod
     def example(cls, O: List[List[float]], E: List[List[float]]) -> float:
