386127362b84f35347a5f67e891983e702ba3d90
oliverweissl <o.weissl@vu.nl>
2023-04-12 17:41:26 +0200

    updated batch starting

--- run_batch.py
+++ run_batch.py
@@ -3,11 +3,15 @@ import asyncio
 from experiment import optimize
 
 
-async def main(novelty_weight = None):
-    await optimize.main(novelty_weight=novelty_weight)
+async def main(novelty_weight = None, amt = 1):
+    for _ in range(amt):
+    	await optimize.main(novelty_weight=novelty_weight)
 
 
 if __name__ == '__main__':
     novelty_weight = float(sys.argv[1])
-    asyncio.run(main(novelty_weight))
-
+    if len(sys.argv) > 2: 
+        amt = int(sys.argv[2])
+        asyncio.run(main(novelty_weight, amt))
+    else:
+        asyncio.run(main(novelty_weight))
---
+++ start_batches.sh
@@ -0,0 +1,8 @@
+python3 -m run_batch 0.0 &
+python3 -m run_batch 0.2 &
+python3 -m run_batch 0.4 &
+python3 -m run_batch 0.6 &
+python3 -m run_batch 0.8 &
+python3 -m run_batch 1.0 &
+python3 -m run_batch &
+wait 
