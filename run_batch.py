b0953f5120d61734bb0682a65356e1adffc69747
oliverweissl <o.weissl@vu.nl>
2023-04-17 08:45:41 +0200

    upd batch starting - supports seeding

--- run_batch.py
+++ run_batch.py
@@ -1,17 +1,21 @@
-import sys
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import asyncio
 from experiment import optimize
 
+parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+parser.add_argument("-n", "--novelty", default=None,
+                    help="Weight of novelty for averaging. If None: penalizing fitness")
+parser.add_argument("-a", "--amount", default=1, type=int, help="Amount of simulations per config.")
+parser.add_argument("-s", "--seed", default=1234, type=int, help="Set seed for reproducability.")
 
-async def main(novelty_weight: "float|None" = None, amt: int = 1):
-    for _ in range(amt):
-        await optimize.main(novelty_weight=novelty_weight)
+args = vars(parser.parse_args())
+
+
+async def main(novelty_weight: "float|None", amt: int, seed_part:int):
+    if novelty_weight is not None:
+        novelty_weight = float(novelty_weight)
+    for i in range(amt):
+        await optimize.main(novelty_weight=novelty_weight, seed_val=int(f"{seed_part}{i}"))
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        asyncio.run(main(float(sys.argv[1]), int(sys.argv[2])))
-    else:
-        if len(sys.argv) > 1:
-            asyncio.run(main(amt=int(sys.argv[1])))
-        else:
-            asyncio.run(main())
+    asyncio.run(main(args["novelty"], args["amount"], args["seed"]))
--- start_batch.sh
+++ start_batch.sh
@@ -1,10 +1,10 @@
-for run in {1..5}; do
-  python3 -m run_batch 0.0 &
-  python3 -m run_batch 0.25 &
-  python3 -m run_batch 0.5 &
-  python3 -m run_batch 0.75 &
-  python3 -m run_batch 1.0 &
-  python3 -m run_batch &
-  sleep 1
+for run in {1..2}; do
+  python3 -m run_batch -n 0.0 -s run &
+  python3 -m run_batch -n 0.25 -s run &
+  python3 -m run_batch -n 0.5 -s run &
+  python3 -m run_batch -n 0.75 -s run &
+  python3 -m run_batch -n 1.0 -s run &
+  python3 -m run_batch -s run &
+  sleep 60
 done
 wait
